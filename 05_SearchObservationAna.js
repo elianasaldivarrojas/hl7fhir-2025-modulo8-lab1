fa630c84f1073252c870c0c0676d112b3707ee94
Flora Eliana Saldivar Rojas <esaldivar@inen.sld.pe>
2025-09-16 17:17:58 -0500

    Actualizar Laboratorio 8 - Eliana Saldivar

--- 05_SearchObservationAna.js
+++ 05_SearchObservationAna.js
@@ -1,18 +1,148 @@
-https://hapi.fhir.org/baseR5/Observation?subject=patient/810085
+https://hapi.fhir.org/baseR5/Observation?subject=Patient/810085
 
 /*Resultado de la consulta */
 {
     "resourceType": "Bundle",
-    "id": "c2fa873b-238c-4760-8371-10e0daa4ac02",
+    "id": "a17a6109-079a-4f40-b929-beb2a4124c0a",
     "meta": {
-        "lastUpdated": "2025-09-15T23:35:14.913+00:00"
+        "lastUpdated": "2025-09-16T22:15:42.485+00:00"
     },
     "type": "searchset",
-    "total": 0,
+    "total": 2,
     "link": [
         {
             "relation": "self",
-            "url": "https://hapi.fhir.org/baseR5/Observation?subject=patient%2F810085"
+            "url": "https://hapi.fhir.org/baseR5/Observation?subject=Patient%2F810085"
+        }
+    ],
+    "entry": [
+        {
+            "fullUrl": "https://hapi.fhir.org/baseR5/Observation/810087",
+            "resource": {
+                "resourceType": "Observation",
+                "id": "810087",
+                "meta": {
+                    "versionId": "1",
+                    "lastUpdated": "2025-09-15T23:34:38.687+00:00",
+                    "source": "#RDSXWSc36T25qAxW"
+                },
+                "status": "final",
+                "category": [
+                    {
+                        "coding": [
+                            {
+                                "system": "http://terminology.hl7.org/CodeSystem/observation-category",
+                                "code": "vital-signs",
+                                "display": "Vital Signs"
+                            }
+                        ]
+                    }
+                ],
+                "code": {
+                    "coding": [
+                        {
+                            "system": "http://loinc.org",
+                            "code": "85354-9",
+                            "display": "Blood pressure panel"
+                        }
+                    ]
+                },
+                "subject": {
+                    "reference": "Patient/810085"
+                },
+                "encounter": {
+                    "reference": "Encounter/810086"
+                },
+                "effectiveDateTime": "2025-09-01T09:10:00Z",
+                "component": [
+                    {
+                        "code": {
+                            "coding": [
+                                {
+                                    "system": "http://loinc.org",
+                                    "code": "8480-6",
+                                    "display": "Systolic blood pressure"
+                                }
+                            ]
+                        },
+                        "valueQuantity": {
+                            "value": 120,
+                            "unit": "mmHg",
+                            "system": "http://unitsofmeasure.org",
+                            "code": "mm[Hg]"
+                        }
+                    },
+                    {
+                        "code": {
+                            "coding": [
+                                {
+                                    "system": "http://loinc.org",
+                                    "code": "8462-4",
+                                    "display": "Diastolic blood pressure"
+                                }
+                            ]
+                        },
+                        "valueQuantity": {
+                            "value": 80,
+                            "unit": "mmHg",
+                            "system": "http://unitsofmeasure.org",
+                            "code": "mm[Hg]"
+                        }
+                    }
+                ]
+            },
+            "search": {
+                "mode": "match"
+            }
+        },
+        {
+            "fullUrl": "https://hapi.fhir.org/baseR5/Observation/810088",
+            "resource": {
+                "resourceType": "Observation",
+                "id": "810088",
+                "meta": {
+                    "versionId": "1",
+                    "lastUpdated": "2025-09-15T23:34:38.687+00:00",
+                    "source": "#RDSXWSc36T25qAxW"
+                },
+                "status": "final",
+                "category": [
+                    {
+                        "coding": [
+                            {
+                                "system": "http://terminology.hl7.org/CodeSystem/observation-category",
+                                "code": "laboratory",
+                                "display": "Laboratory"
+                            }
+                        ]
+                    }
+                ],
+                "code": {
+                    "coding": [
+                        {
+                            "system": "http://loinc.org",
+                            "code": "2339-0",
+                            "display": "Glucose [Mass/volume] in Blood"
+                        }
+                    ]
+                },
+                "subject": {
+                    "reference": "Patient/810085"
+                },
+                "encounter": {
+                    "reference": "Encounter/810086"
+                },
+                "effectiveDateTime": "2025-09-01T09:15:00Z",
+                "valueQuantity": {
+                    "value": 90,
+                    "unit": "mg/dL",
+                    "system": "http://unitsofmeasure.org",
+                    "code": "mg/dL"
+                }
+            },
+            "search": {
+                "mode": "match"
+            }
         }
     ]
 }
--- 06_SearchObservationLoinc.js
+++ 06_SearchObservationLoinc.js
@@ -1,17 +1,98 @@
-https://hapi.fhir.org/baseR5/Observation?code=http://loinc.org|85354-9&subject=patient/810085
+https://hapi.fhir.org/baseR5/Observation?code=http://loinc.org|85354-9&subject=Patient/810085
 
 {
     "resourceType": "Bundle",
-    "id": "4d39e9e1-50af-4d1f-947c-0b21d5ff9d65",
+    "id": "e05f7c49-5c24-4961-8a8f-dea7428a935c",
     "meta": {
-        "lastUpdated": "2025-09-15T23:35:30.461+00:00"
+        "lastUpdated": "2025-09-16T22:16:01.533+00:00"
     },
     "type": "searchset",
-    "total": 0,
+    "total": 1,
     "link": [
         {
             "relation": "self",
-            "url": "https://hapi.fhir.org/baseR5/Observation?code=http%3A%2F%2Floinc.org%7C85354-9&subject=patient%2F810085"
+            "url": "https://hapi.fhir.org/baseR5/Observation?code=http%3A%2F%2Floinc.org%7C85354-9&subject=Patient%2F810085"
+        }
+    ],
+    "entry": [
+        {
+            "fullUrl": "https://hapi.fhir.org/baseR5/Observation/810087",
+            "resource": {
+                "resourceType": "Observation",
+                "id": "810087",
+                "meta": {
+                    "versionId": "1",
+                    "lastUpdated": "2025-09-15T23:34:38.687+00:00",
+                    "source": "#RDSXWSc36T25qAxW"
+                },
+                "status": "final",
+                "category": [
+                    {
+                        "coding": [
+                            {
+                                "system": "http://terminology.hl7.org/CodeSystem/observation-category",
+                                "code": "vital-signs",
+                                "display": "Vital Signs"
+                            }
+                        ]
+                    }
+                ],
+                "code": {
+                    "coding": [
+                        {
+                            "system": "http://loinc.org",
+                            "code": "85354-9",
+                            "display": "Blood pressure panel"
+                        }
+                    ]
+                },
+                "subject": {
+                    "reference": "Patient/810085"
+                },
+                "encounter": {
+                    "reference": "Encounter/810086"
+                },
+                "effectiveDateTime": "2025-09-01T09:10:00Z",
+                "component": [
+                    {
+                        "code": {
+                            "coding": [
+                                {
+                                    "system": "http://loinc.org",
+                                    "code": "8480-6",
+                                    "display": "Systolic blood pressure"
+                                }
+                            ]
+                        },
+                        "valueQuantity": {
+                            "value": 120,
+                            "unit": "mmHg",
+                            "system": "http://unitsofmeasure.org",
+                            "code": "mm[Hg]"
+                        }
+                    },
+                    {
+                        "code": {
+                            "coding": [
+                                {
+                                    "system": "http://loinc.org",
+                                    "code": "8462-4",
+                                    "display": "Diastolic blood pressure"
+                                }
+                            ]
+                        },
+                        "valueQuantity": {
+                            "value": 80,
+                            "unit": "mmHg",
+                            "system": "http://unitsofmeasure.org",
+                            "code": "mm[Hg]"
+                        }
+                    }
+                ]
+            },
+            "search": {
+                "mode": "match"
+            }
         }
     ]
 }
